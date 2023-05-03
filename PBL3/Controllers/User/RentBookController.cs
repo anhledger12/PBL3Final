7c7074956f51c7978474f8b1f4b5c8212be788db
anhledger12 <nguyenanhnk10@gmail.com>
2023-05-03 17:46:09 +0700

    Them gio hang

--- PBL3/Controllers/User/RentBookController.cs
+++ PBL3/Controllers/User/RentBookController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
 using PBL3.Data;
+using PBL3.Models.Entities;
 
 namespace PBL3.Controllers.User
 {
@@ -11,9 +13,43 @@ namespace PBL3.Controllers.User
          * Đây là Controller thực hiện tác vụ khi điều hướng tới 
          * 
          */
+        LibraryManagementContext _context;
+        public RentBookController(LibraryManagementContext context) 
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        //Xem giỏ hàng
+        public async Task<IActionResult> ViewCart()
+        {
+            string AccName = User.Identity.Name;
+            try
+            {
+                var Cart = (from Titles in _context.Titles
+                            where (from Book in _context.Books
+                                   join RentDetail in _context.BookRentDetails on Book.IdBook equals RentDetail.IdBook
+                                   join BookRental in _context.BookRentals on RentDetail.IdBookRental equals BookRental.Id
+                                   where Book.IdTitle == Titles.IdTitle && BookRental.AccSending == AccName
+                                   select Book).Any()
+                            select new Title
+                            {
+                                IdTitle = Titles.IdTitle,
+                                NameBook = Titles.NameBook
+                            }).ToList();
+                if (Cart == null)
+                {
+                    return View("/Views/BookRentals/ViewCart.cshtml", "Không có đơn nào chưa gửi");
+                }
+                return View("/Views/BookRentals/ViewCart.cshtml",Cart);
+            }
+            catch (Exception e)
+            {
+                return NotFound();
+            }
         }
     }
 }
