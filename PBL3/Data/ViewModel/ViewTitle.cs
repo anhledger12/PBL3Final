4b2151ffc43fa4300a41969524548f88b81e7fe4
tosanoob <ctruongtan31070901@gmail.com>
2023-05-04 17:41:38 +0700

    Add TimeApprove

--- PBL3/Data/ViewModel/ViewTitle.cs
+++ PBL3/Data/ViewModel/ViewTitle.cs
@@ -3,6 +3,7 @@
     public class ViewTitle
     { 
         public string? IdTitle { get; set; }
+        public string? IdBook { get; set; }
         public string? NameBook { get; set; }
         public string? NameWriter { get; set; }
         public string? NameBookshelf { get; set; }
