6fdafa3af6aa66d4ed44a7fb461dffe9b87728de
anhledger12 <nguyenanhnk10@gmail.com>
2023-05-04 08:20:21 +0700

    Delete BookRental statesend == false when no BookRentDetail

--- PBL3/Controllers/User/RentBookController.cs
+++ PBL3/Controllers/User/RentBookController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+using Microsoft.EntityFrameworkCore;
 using PBL3.Data;
 using PBL3.Models.Entities;
+using System.Collections.Specialized;
+using System.Net.Security;
 
 namespace PBL3.Controllers.User
 {
@@ -29,7 +32,7 @@ namespace PBL3.Controllers.User
             string AccName = User.Identity.Name;
             try
             {
-                var Cart = _context.Titles
+                var Cart = await _context.Titles
                                     .Where(Titles => _context.Books
                                     .Any(Book => _context.BookRentDetails
                                         .Any(RentDetail => _context.BookRentals
@@ -41,7 +44,7 @@ namespace PBL3.Controllers.User
                                      {
                                          IdTitle = Titles.IdTitle,
                                          NameBook = Titles.NameBook
-                                     }).ToList();
+                                     }).ToListAsync();
                 if (Cart == null)
                 {
                     return View("/Views/BookRentals/ViewCart.cshtml", "Không có đơn nào chưa gửi");
@@ -53,28 +56,41 @@ namespace PBL3.Controllers.User
                 return NotFound();
             }
         }
-        [HttpGet]
-        public IActionResult Delete(string id)
+        public async Task<IActionResult> Delete(string id)
         {
             string AccName = User.Identity.Name;
-            BookRentDetail s = (from brd in _context.BookRentDetails
-                                join br in _context.BookRentals on brd.IdBookRental equals br.Id
-                                join b in _context.Books on brd.IdBook equals b.IdBook
-                                where b.IdTitle == id && br.StateSend == false && br.AccSending == AccName
-                                select new BookRentDetail
-                                {
-                                    IdBookRental = brd.IdBookRental,
-                                    IdBook = brd.IdBook,
-                                    StateReturn = brd.StateReturn,
-                                    StateTake = brd.StateTake,
-                                    ReturnDate = brd.ReturnDate
-                                }).FirstOrDefault();
+            BookRentDetail s = await (from brd in _context.BookRentDetails
+                                      join br in _context.BookRentals on brd.IdBookRental equals br.Id
+                                      join b in _context.Books on brd.IdBook equals b.IdBook
+                                      where b.IdTitle == id && br.StateSend == false && br.AccSending == AccName
+                                      select new BookRentDetail
+                                      {
+                                          IdBookRental = brd.IdBookRental,
+                                          IdBook = brd.IdBook,
+                                          StateReturn = brd.StateReturn,
+                                          StateTake = brd.StateTake,
+                                          ReturnDate = brd.ReturnDate
+                                      }).FirstOrDefaultAsync();
             if (s == null)
             {
                 return NotFound();
             }
             _context.BookRentDetails.Remove(s);
             _context.SaveChanges();
+            int numRentDetail = (from brd in _context.BookRentDetails
+                                         join br in _context.BookRentals on brd.IdBookRental equals br.Id
+                                         where br.StateSend == false && br.AccSending == AccName
+                                         select brd.IdBook
+                                         ).ToList().Count();
+            if (numRentDetail == 0)
+            {
+                //xóa BookRental
+                BookRental bookRental = await (from br in _context.BookRentals
+                                        where br.StateSend == false && br.AccSending == AccName
+                                        select br).FirstOrDefaultAsync();
+                _context.BookRentals.Remove(bookRental);
+                _context.SaveChanges();
+            }
             return RedirectToAction("ViewCart");
         }
     }
