a5bb8d545ed6edd25aab7f48762b70cc8d5aeb34
anhledger12 <nguyenanhnk10@gmail.com>
2023-05-05 02:19:13 +0700

    Send Cart

--- PBL3/Controllers/User/RentBookController.cs
+++ PBL3/Controllers/User/RentBookController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor.Compilation;
 using Microsoft.EntityFrameworkCore;
+using PBL3.Controllers.Anonymous;
 using PBL3.Data;
+using PBL3.Models;
 using PBL3.Models.Entities;
 using System.Collections.Specialized;
 using System.Net.Security;
@@ -27,35 +29,49 @@ namespace PBL3.Controllers.User
         }
 
         //Xem giỏ hàng
+
+        [Authorize(Roles = UserRole.User)]
         public async Task<IActionResult> ViewCart()
         {
+            var check = (from br in _context.BookRentals
+                         where br.StateSend == false
+                         select br).FirstOrDefault();
+            if (check == null)
+            {
+                return View("/Views/BookRentals/ViewCartEmpty.cshtml");
+            }
             string AccName = User.Identity.Name;
             try
             {
-                var Cart = await _context.Titles
-                                    .Where(Titles => _context.Books
-                                    .Any(Book => _context.BookRentDetails
-                                        .Any(RentDetail => _context.BookRentals
-                                            .Any(BookRental => BookRental.Id == RentDetail.IdBookRental &&
-                                                               RentDetail.IdBook == Book.IdBook &&
-                                                               Book.IdTitle == Titles.IdTitle &&
-                                                               BookRental.AccSending == AccName))))
-                                     .Select(Titles => new Title
-                                     {
-                                         IdTitle = Titles.IdTitle,
-                                         NameBook = Titles.NameBook
-                                     }).ToListAsync();
+                IEnumerable<BookRental> bookRentals = _context.BookRentals.ToList();
+                IEnumerable<BookRentDetail> bookRentDetails = _context.BookRentDetails.ToList();
+                IEnumerable<Book> books = _context.Books.ToList();
+                IEnumerable<Title> titles = _context.Titles.ToList();
+                var Cart = (from brd in bookRentDetails
+                           join br in bookRentals on brd.IdBookRental equals br.Id
+                           join b in books on brd.IdBook equals b.IdBook
+                           join Titles in titles on b.IdTitle equals Titles.IdTitle
+                           where br.AccSending == AccName && br.StateSend == false
+                           select new RentModel
+                           {
+                               bookRentDetail = brd,
+                               bookRental = br,
+                               book = b,
+                               title = Titles
+                           }).ToList();
                 if (Cart == null)
                 {
                     return View("/Views/BookRentals/ViewCart.cshtml", "Không có đơn nào chưa gửi");
                 }
                 return View("/Views/BookRentals/ViewCart.cshtml",Cart);
             }
-            catch (Exception e)
+            catch
             {
                 return NotFound();
             }
         }
+
+        [Authorize(Roles = UserRole.User)]
         public async Task<IActionResult> Delete(string id)
         {
             string AccName = User.Identity.Name;
@@ -92,6 +108,23 @@ namespace PBL3.Controllers.User
                 _context.SaveChanges();
             }
             return RedirectToAction("ViewCart");
+        }
+
+        [Authorize(Roles = UserRole.User)]
+        public async Task<IActionResult> Sendrent()
+        {
+            if (!ModelState.IsValid)
+            {
+                return NotFound();
+            }
+            var check = (from br in _context.BookRentals
+                        where br.StateSend == false
+                        select br).FirstOrDefault();
+            check.StateSend = true;
+            _context.Update<BookRental>(check);
+            _context.SaveChanges();
+            return View("/Views/Home/Index.cshtml");
+            //return id.ToString();
         }
     }
 }
---
+++ PBL3/Models/RentModel.cs
@@ -0,0 +1,12 @@
+﻿using PBL3.Models.Entities;
+
+namespace PBL3.Models
+{
+    public class RentModel
+    {
+        public Title title { get; set; }
+        public BookRental bookRental { get; set; }
+        public BookRentDetail bookRentDetail { get; set; }
+        public Book book { get; set; }
+    }
+}
