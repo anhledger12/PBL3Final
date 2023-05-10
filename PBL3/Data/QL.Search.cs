aaa12e94e746434d91c4e0988d794206f5434aa0
anhledger12 <nguyenanhnk10@gmail.com>
2023-05-10 23:59:23 +0700

    Move to QL Search

--- PBL3/Data/QL.Search.cs
+++ PBL3/Data/QL.Search.cs
@@ -1,7 +1,28 @@
-﻿namespace PBL3.Data
+﻿using PBL3.Models.Entities;
+
+namespace PBL3.Data
 {
     public partial class QL
     {
-        // Quản lý tìm kiếm (search), Nguyên Anh làm
+        public List<Account> GetAccounts(string name, bool stateActive = true)
+        {
+           return _context.Accounts
+                        .Select(x => x)
+                        .Where(x => x.AccName.Contains(name) && x.Active == stateActive).ToList();
+        }
+        public List<Title> GetTitles(string name, bool stateActive = true)
+        {
+            return _context.Titles
+                         .Select(x => x)
+                         .Where(x => x.NameBook.Contains(name) && x.Active == stateActive).ToList();
+        }
+
+        public string GetNameBook(string a)
+        {
+            if (a.Length == 1)
+                return "";
+            a = a.Remove(a.Length - 1);
+            return a;
+        }
     }
 }
