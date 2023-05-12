ed7725e6c98debd6cfd5f164c040ac4fb7740584
Artteiv <sontranviet21@gmail.com>
2023-05-13 00:26:18 +0700

    Update newsFeed

--- PBL3/Controllers/AdminAndStaff/NewsFeedsController.cs
+++ PBL3/Controllers/AdminAndStaff/NewsFeedsController.cs
@@ -14,27 +14,23 @@ namespace PBL3.Controllers.AdminAndStaff
     [Authorize(Roles = UserRole.Admin)]
     public class NewsFeedsController : Controller
     {
-        private readonly LibraryManagementContext _context;
-        public NewsFeedsController(LibraryManagementContext context)
+        private readonly QL db;
+        public NewsFeedsController( QL db)
         {
-            _context = context;
+            this.db = db;
         }
         public async Task<IActionResult> Index(int page=1)
         {
             // code phân trang
-            ViewBag.PageCount = (_context.NewsFeeds.Count()+4) / 5;
+            ViewBag.PageCount = (db.GetNewsFeeds().Count()+4) / 5;
             ViewBag.CurrentPage = page;
-            var res = await _context.NewsFeeds.OrderByDescending(p=>p.Id).Skip(page * 5 - 5).Take(5).ToListAsync();
+            var res = await db.GetNewsFeeds().Skip(page * 5 - 5).Take(5).ToListAsync();
             return View(res);
         }
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Detail(int id)
         {
             // detail ổn, chỉ cần sửa lại một chút ở giao diên
-            if (id == null || _context.NewsFeeds == null)
-            {
-                return View("NotFound");
-            }
-            var newsFeed = await _context.NewsFeeds.FirstOrDefaultAsync(m => m.Id == id);
+            var newsFeed = db.GetNewsFeed(id);
             if (newsFeed == null)
             {
                 return View("NotFound");
@@ -53,21 +49,16 @@ namespace PBL3.Controllers.AdminAndStaff
             // create post thì không cần sửa gì, đơn giản rồi
             if (ModelState.IsValid)
             {
-                _context.Add(newsFeed);
-                await _context.SaveChangesAsync();
+                await db.AddNewsFeed(newsFeed);
                 return RedirectToAction("Index");
             }
             return View(newsFeed);
         }
 
-        public async Task<IActionResult> Edit(int? id)
+        public async Task<IActionResult> Edit(int id)
         {
             // Edit cũng giống như create, không cần sửa nhiều, cùng lắm giao diện thôi
-            if (id == null || _context.NewsFeeds == null)
-            {
-                return NotFound();
-            }
-            var newsFeed = await _context.NewsFeeds.FindAsync(id);
+            var newsFeed = db.GetNewsFeed((int)id);
             if (newsFeed == null)
             {
                 return NotFound();
@@ -77,33 +68,21 @@ namespace PBL3.Controllers.AdminAndStaff
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id,NewsFeed newsFeed)
+        public async Task<IActionResult> Edit(NewsFeed newsFeed,int id)
         {
             // ok
-            var res = _context.NewsFeeds.Where(p => p.Id == id);
-            if (res.Count()==0)
-            {
-                return View("NotFound");
-            }
             if (ModelState.IsValid)
             {
-
-                _context.Update(newsFeed);
-                await _context.SaveChangesAsync();                
+                await db.UpdateNewsFeed(newsFeed);                               
                 return RedirectToAction("Index");
             }
             return View(newsFeed);
         }
-        [HttpPost]
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Delete(int id)
         {
             // ok
-            var newsFeed = _context.NewsFeeds.Find(id);
-            if (newsFeed != null)
-            {
-                _context.NewsFeeds.Remove(newsFeed);
-            }
-            await _context.SaveChangesAsync();
+            var newsFeed = db.GetNewsFeed((int)id);
+            await db.RemoveNewsFeed(newsFeed);
             return RedirectToAction("Index");
         }
     }
