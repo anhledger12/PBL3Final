df565f35cc8432a48b9785f93f0d19ef6983c4b8
Artteiv <sontranviet21@gmail.com>
2023-05-13 22:04:27 +0700

    Update bug

--- PBL3/Data/ViewModel/AdminAccountVM.cs
+++ PBL3/Data/ViewModel/AdminAccountVM.cs
@@ -6,6 +6,9 @@ namespace PBL3.Data.ViewModel
     {
         //
         public Account Account { get; set; } = new Account();
+        [Required(ErrorMessage = "Hãy nhập mật khẩu")]
+        [MinLength(5, ErrorMessage = "Mật khẩu phải có ít nhất 5 ký tự")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         public string Role { get; set; }
     }
